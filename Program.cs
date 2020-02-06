485fac32bba49ce177e9938982e18ae73b7e6df1
Jo <Fig@LAPTOP-GKDM7APE>
2020-02-06 17:27:18 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Methods
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Theresult(); // I am calling the method that I created right here
+            Console.ReadKey(true); // it waits for a key to be pushed
+
+        }
+
+        public static void Theresult() // I have created my own method here
+        {
+            Console.Write("Hello, please enter your first and last name, separated by a space." + "\n" + "\n"); // prompts the user
+            string input = Console.ReadLine(); // input is recorded
+            Console.WriteLine("\n" + "Hello " + input + ", press any key to exit now."); // output
+        }
+
+    } // end of program
+}
