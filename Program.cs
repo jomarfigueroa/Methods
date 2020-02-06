7f4a41b2b7b03f487ce11fabef05715dbb619562
Jo <Fig@LAPTOP-GKDM7APE>
2020-02-06 17:40:53 -0500

    commit

--- Program.cs
+++ Program.cs
@@ -17,8 +17,17 @@ namespace Methods
         public static void Theresult() // I have created my own method here
         {
             Console.Write("Hello, please enter your first and last name, separated by a space." + "\n" + "\n"); // prompts the user
-            string input = Console.ReadLine(); // input is recorded
-            Console.WriteLine("\n" + "Hello " + input + ", press any key to exit now."); // output
+            string input = Console.ReadLine();
+            // input is recorded
+
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Invalid input. Push a key to exit.");
+            }
+            else
+            {
+                Console.WriteLine("\n" + "Hello " + input +  "!" + "\n" + "\n" + "Press any key to exit now."); // output
+            }
         }
 
     } // end of program here
